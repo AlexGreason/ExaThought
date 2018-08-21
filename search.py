46bdda8dc44d665c70c07d5a8a0eca9fa54923d2
exa <quantum935@yahoo.com>
2018-08-20 23:00:57 -0500

    added """"uci"""" """"""support""""""

--- search.py
+++ search.py
@@ -6,11 +6,9 @@ from chess.polyglot import zobrist_hash as zhash
 
 
 scores = {'0-1':float("inf"), '1-0':-float("inf"), "1/2-1/2":0}
-nodes = 0
 
 def alphabeta(board, depth, alpha, beta, transtable):
-    global nodes
-    nodes += 1
+    totalnodes = 0
     hash = zhash(board)
     if (hash, depth) in transtable:
         return transtable[(hash, depth)]
@@ -18,13 +16,14 @@ def alphabeta(board, depth, alpha, beta, transtable):
     #random.shuffle(moves)
     if len(moves) == 0:
         result = board.result()
-        return scores[result], []
+        return scores[result], [], 1
     if depth == 0:
-        return evaluateBoard(board), []
+        return evaluateBoard(board), [], 1
     ourpv = []
     for move in moves:
         board.push(move)
-        value, pv = alphabeta(board, depth - 1, -beta, -alpha, transtable)
+        value, pv, nodes = alphabeta(board, depth - 1, -beta, -alpha, transtable)
+        totalnodes += nodes
         value *= -1
         board.pop()
         if value > alpha or len(ourpv) == 0:
@@ -32,14 +31,14 @@ def alphabeta(board, depth, alpha, beta, transtable):
             ourpv = pv
         alpha = max(alpha, value)
         if alpha >= beta:
-            transtable[(hash, depth)] = beta, ourpv
-            return beta, ourpv
-    transtable[(hash, depth)] = alpha, ourpv
-    return alpha, ourpv
+            transtable[(hash, depth)] = beta, ourpv, totalnodes
+            return beta, ourpv, totalnodes
+    transtable[(hash, depth)] = alpha, ourpv, totalnodes
+    return alpha, ourpv, totalnodes
 
 if __name__ == "__main__":
     starttime = time.time()
-    value, pv = alphabeta(chess.Board("8/6K1/1p1B1RB1/8/2Q5/2n1kP1N/3b4/4n3 w - - 0 1"), 4, -float("inf"), float("inf"), {})
+    value, pv, nodes = alphabeta(chess.Board("8/6K1/1p1B1RB1/8/2Q5/2n1kP1N/3b4/4n3 w - - 0 1"), 4, -float("inf"), float("inf"), {})
     print(value, pv)
     endtime = time.time()
     print(nodes, endtime-starttime)
